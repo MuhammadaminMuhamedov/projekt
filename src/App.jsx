81f33a14496095bf5dd8bb73e59856075a7ba647
MuhammadaminMuhamedov <m32446433@gmail.com>
2025-12-24 14:45:13 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <h1>Welcom to React JS</h1>
+      <h2>This is my first React App</h2>
+      <ul>
+        <li>HTML</li>
+        <li>Css</li>
+        <li>JavaSicript</li>
+        <li>React</li>
+      </ul>
+      <h2>What is React</h2>
+      <p>React is a JavaScript library for building user interfaces.</p>
+      <h2>What is Single page</h2>
+      <p>Single-page applications (SPAs) are web applications that dynamically update the page content without requiring a full page reload.</p>
+    </div>
+  )
+}
+
+export default App
